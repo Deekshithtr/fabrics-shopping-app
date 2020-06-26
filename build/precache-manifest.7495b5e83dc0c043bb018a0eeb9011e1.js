5c3f4d270ce6cfd71751bb68b88a748f1069bfcb
Deekshithtr <deekshithtr11@gmail.com>
2020-06-26 19:41:25 +0530

    first commit

---
+++ build/precache-manifest.7495b5e83dc0c043bb018a0eeb9011e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2275602f641c23cfbd980e7a6bb09834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33da6a72af32abac8ce2",
+    "url": "/static/css/main.a1d5543f.chunk.css"
+  },
+  {
+    "revision": "7bcff0a640f5125595cf",
+    "url": "/static/js/2.46982c0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.46982c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33da6a72af32abac8ce2",
+    "url": "/static/js/main.9287c871.chunk.js"
+  },
+  {
+    "revision": "b6f1cec81779c6c9c593",
+    "url": "/static/js/runtime-main.a172c28f.js"
+  },
+  {
+    "revision": "2048b4d302e3fc4134f135f701e49ad6",
+    "url": "/static/media/kids6.2048b4d3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f61d72c7783ad2de96df01dd5c3ce61",
+    "url": "/static/media/men.7f61d72c.jpg"
+  },
+  {
+    "revision": "016676cbc7ffcf8d3629768dc590c405",
+    "url": "/static/media/women4.016676cb.jpg"
+  }
+]);
